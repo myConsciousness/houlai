41914d5a57ce608f248125bbe0d4959bd492573b
Kato Shinya <yourdream28@gmail.com>
2020-10-07 10:34:29 +0900

    add

---
+++ src/main/java/org/thinkit/houlai/HoulaiApplication.java
@@ -0,0 +1,13 @@
+package org.thinkit.houlai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoulaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HoulaiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/thinkit/houlai/HoulaiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.thinkit.houlai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HoulaiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
