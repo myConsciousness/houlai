1b4c140ecdb8adcdfa3eabbf37089d7df777892c
Kato Shinya <yourdream28@gmail.com>
2020-10-10 17:59:49 +0900

    fixed package

--- src/main/java/org/houlai/HoulaiApplication.java
+++ src/main/java/org/houlai/HoulaiApplication.java
@@ -12,7 +12,7 @@
  * the License.
  */
 
-package org.thinkit.houlai;
+package org.houlai;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
