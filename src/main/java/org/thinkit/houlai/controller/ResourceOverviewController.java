cd275cb4abe97c633365adaa21a7b5cd351f9d0f
Kato Shinya <kato.shinya.dev@gmail.com>
2021-02-09 18:33:42 +0900

    update

--- src/main/java/org/thinkit/houlai/controller/ResourceOverviewController.java
+++ src/main/java/org/thinkit/houlai/controller/ResourceOverviewController.java
@@ -14,8 +14,11 @@
 
 package org.thinkit.houlai.controller;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 
+import org.apache.commons.collections4.map.LinkedMap;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -23,7 +26,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.thinkit.api.catalog.Catalog;
+import org.thinkit.framework.content.ContentInvoker;
 import org.thinkit.houlai.catalog.ResourceType;
+import org.thinkit.houlai.content.ResourceTypeNameLoader;
 import org.thinkit.houlai.entity.Resource;
 import org.thinkit.houlai.form.ResourceOverviewForm;
 import org.thinkit.houlai.service.ResourceService;
@@ -45,7 +50,18 @@ public final class ResourceOverviewController {
 
     @GetMapping("resource/overview")
     public String index(Model model) {
+
+        final Map<String, Object> resourceTypes = new LinkedMap<>();
+
+        Arrays.asList(ResourceType.values()).forEach(resourceType -> {
+            final String ResourceTypeName = ContentInvoker.of(ResourceTypeNameLoader.from(resourceType)).invoke()
+                    .getResourceTypeName();
+            resourceTypes.put(ResourceTypeName, String.valueOf(resourceType.getCode()));
+        });
+
+        model.addAttribute("resourceTypes", resourceTypes);
         model.addAttribute("resourceOverviewForm", ResourceOverviewForm.newInstance());
+
         return "resource_overview";
     }
 
