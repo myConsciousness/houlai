e3f77c8574f01406e1bd0e04ecf6b0d6bbc60383
Kato Shinya <kato.shinya.dev@gmail.com>
2021-02-09 18:33:13 +0900

    add

---
+++ src/main/java/org/thinkit/houlai/content/entity/ResourceTypeName.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2021 Kato Shinya.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package org.thinkit.houlai.content.entity;
+
+import java.io.Serializable;
+
+import org.thinkit.framework.content.entity.ContentEntity;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+/**
+ * コンテンツ「リソース種別名称」の項目を管理するクラスです。
+ *
+ * @author Kato Shinya
+ * @since 1.0.0
+ */
+@ToString
+@EqualsAndHashCode
+@Builder(toBuilder = true)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PRIVATE, force = true)
+public final class ResourceTypeName implements ContentEntity, Serializable {
+
+    /**
+     * シリアルバージョンUID
+     */
+    private static final long serialVersionUID = 1260480518039409822L;
+
+    /**
+     * リソース種別名称
+     */
+    @Getter
+    private String resourceTypeName;
+}
